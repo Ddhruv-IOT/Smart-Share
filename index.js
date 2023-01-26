4f482fc39b107645fda548f810d226c0b67af527
Ddhruv-IOT <ddhruvarora2@gmail.com>
2023-01-26 22:01:27 +0530

    exposed public folder

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true})); 
-
+app.use(express.static(__dirname+'/public/'));
 /* ------------- Text Sharing Engine ------------- */
 
 // see the copied text on server device
