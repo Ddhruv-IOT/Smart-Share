956bf31d3c8a3dee58314d7ab214a1f37e961298
Ddhruv-IOT <ddhruvarora2@gmail.com>
2023-02-08 20:59:50 +0530

    sepearted JS file for uploading files :: pr commit 99

---
+++ public/code/scripts/uploadLogic.js
@@ -0,0 +1,62 @@
+window.onload = ()=> {
+
+    fetch('code/assets/dw.png').then(r => r.blob()).then( imageBlob => {
+        const imageObjectURL = URL.createObjectURL(imageBlob);
+        console.log(imageObjectURL);
+        document.getElementById("img").src = imageObjectURL;
+    })
+
+    var fileArea = document.querySelector("#file-area");
+    var fileInput = document.querySelector('#fileInput');
+    var uploadButton = document.querySelector('#uploadButton');
+    var meter = document.getElementById('rloder');
+    var fileName = document.getElementById('fileName');
+    meter.style.display = "none";
+    fileName.innerText = "";
+
+    fileArea.addEventListener('change', function() {
+         meter.style.display = "none"
+         var len = fileInput.files.length 
+         fileName.innerText = (len <= 1  ? `Selected: ${fileInput.files[0]['name']}` : `Selected: ${len} files`) 
+    })
+
+    uploadButton.addEventListener('click', function() {
+        var formData = new FormData();
+        var files = fileInput.files;
+
+        if (files.length != 0) {
+            for (var i = 0; i < files.length; i++) {
+                formData.append("f", files[i]);
+            }
+
+            var xhr = new XMLHttpRequest();
+            xhr.open('POST', '/upload', true);
+
+            xhr.upload.onprogress = function(event) {
+                // Handle progress events
+                meter.style.display = "block"
+                var len = (event.loaded /event.total) * 90
+                meter.style.width = len + "%";
+                console.log((event.loaded /event.total) * 100)
+            };
+
+            xhr.onload = function(event) {
+                if(xhr.status == 200) {
+                    // success Block
+                    fileName.innerText = "Upload Successful!";
+                    fileInput.value = '';
+                } else {
+                    fileName.innerText = "";
+                    meter.style.display = "none"
+                    alert("Failed")
+                }
+                console.log(xhr.status)
+            };
+            xhr.send(formData);
+        } else {
+            alert("No files")
+            meter.style.display = "none";
+            fileName.innerText = "";
+        }
+    });
+}
