501d02ea9aacfe31626509f4dffe320152c0b3ba
Ddhruv-IOT <ddhruvarora2@gmail.com>
2025-07-12 16:31:26 +0530

    Termux copy support under testing

--- controllers/textEngine/getServerClipoboard.js
+++ controllers/textEngine/getServerClipoboard.js
@@ -1,6 +1,14 @@
 const spawn = require("child_process").spawn;
+const Clipboard_Mode = require("dotenv").config().parsed.CLIPBOARD_MODE
+const { clipboardDataRef } = require("./writeServerClipboard");
 
 var getServerCp = (req, res) => {
+    if (Clipboard_Mode === "DEDICATED") {
+        const data = clipboardDataRef();
+        console.log(data)
+        if (!data) return res.status(404).send('No clipboard data available');
+        return res.status(200).send(data);
+    }
     const pyProg = spawn('python',["./python-plugins/get_server_clipboard.py"]);
     pyProg.stdout.on('data', function(data) {  
       res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' })
--- controllers/textEngine/writeServerClipboard.js
+++ controllers/textEngine/writeServerClipboard.js
@@ -1,8 +1,20 @@
+// const { text } = require("stream/consumers");
+
 const spawn = require("child_process").spawn;
+const Clipboard_Mode = require("dotenv").config().parsed.CLIPBOARD_MODE
+
+let clipboardData = '';
 
 // process the data from pastclipboard or direct request for copying text
 writeServerCp = (req, res) => {
     text = req.query.x;
+
+    if (Clipboard_Mode === "DEDICATED") {
+          clipboardData = text;
+          res.status(204)
+          return res.end('end');
+    }
+    
     const pyProg = spawn('python', ["./python-plugins/write_server_clipboard.py", text]);
     pyProg.stdout.on('data', function(data) {  
       res.write(data);
@@ -11,4 +23,4 @@ writeServerCp = (req, res) => {
     res.status(204);
   }
 
-  module.exports = writeServerCp
+  module.exports = { writeServerCp, clipboardDataRef: () => clipboardData };
