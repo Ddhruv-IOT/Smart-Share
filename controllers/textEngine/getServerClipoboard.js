425303ed8543839a216750f35eb2e83efaa70a1d
Ddhruv-IOT <ddhruvarora2@gmail.com>
2025-07-12 14:51:39 +0530

    Fixes #4: Bug fixed and enhancement added, emoji support

--- controllers/textEngine/getServerClipoboard.js
+++ controllers/textEngine/getServerClipoboard.js
@@ -3,8 +3,8 @@ const spawn = require("child_process").spawn;
 var getServerCp = (req, res) => {
     const pyProg = spawn('python',["./python-plugins/get_server_clipboard.py"]);
     pyProg.stdout.on('data', function(data) {  
-      res.write(data);
-      res.end();
+      res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' })
+      res.end(data.toString('utf8'));
     });
   }
 
